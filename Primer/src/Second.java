a35560231670b7b8e550e379110d03ebb99e9fd9
Lucas Conde <59385581+Parkourcondexd1@users.noreply.github.com>
2021-12-23 11:06:38 -0300

    mas

--- Primer/src/Second.java
+++ Primer/src/Second.java
@@ -1,56 +1,168 @@
+import java.util.Arrays;
 
 public class Second {
-	
+
 	/*
-	Complete the function that accepts a string parameter, and reverses each word in the string. 
-	All spaces in the string should be retained.
-	*/
-	
-	public static String reverseWords(final String original){
-	  String[] arr  = original.split(" ");
-	    
-	  if ( arr.length == 0 ) {
-	    return original;
-	  }
-	    
-	  String resultado = ""; 
-	    
-	  for ( String word : arr ) {
-	    StringBuilder input = new StringBuilder();
-	    resultado += input.append(word).reverse().toString() + " ";
-	  }
-	    
-	  return resultado.trim();
+	 * Complete the function that accepts a string parameter, and reverses each word
+	 * in the string. All spaces in the string should be retained.
+	 */
+
+	public static String reverseWords(final String original) {
+		String[] arr = original.split(" ");
+
+		if (arr.length == 0) {
+			return original;
+		}
+
+		String resultado = "";
+
+		for (String word : arr) {
+			StringBuilder input = new StringBuilder();
+			resultado += input.append(word).reverse().toString() + " ";
+		}
+
+		return resultado.trim();
 	}
 
 	/*
-	 A square of squares
-	You like building blocks. You especially like building blocks that are squares. And what you even like more, 
-	is to arrange them into a square of square building blocks!
+	 * A square of squares You like building blocks. You especially like building
+	 * blocks that are squares. And what you even like more, is to arrange them into
+	 * a square of square building blocks!
+	 * 
+	 * However, sometimes, you can't arrange them into a square. Instead, you end up
+	 * with an ordinary rectangle! Those blasted things! If you just had a way to
+	 * know, whether you're currently working in vain… Wait! That's it! You just
+	 * have to check if your number of building blocks is a perfect square.
+	 */
 
-	However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! 
-	Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! 
-	You just have to check if your number of building blocks is a perfect square.
-	*/
-	
-	public static boolean isSquare(int n) {        
-	      
-	      if ( n < 0 ) {
-	        return false;
-	      }
-	      
-	      Double raiz = Math.sqrt( n );
-	      
-	      if ( raiz % 1 == 0 ){
-	        return true;
-	      }else{
-	        return false;
-	      }
-	            
-	    }
-	
+	public static boolean isSquare(int n) {
+
+		if (n < 0) {
+			return false;
+		}
+
+		Double raiz = Math.sqrt(n);
+
+		if (raiz % 1 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	/*
+	 * You are going to be given a word. Your job is to return the middle character
+	 * of the word. If the word's length is odd, return the middle character. If the
+	 * word's length is even, return the middle 2 characters.
+	 */
+
+	public static String getMiddle(String word) {
+		int largo = word.length();
+
+		if (largo % 2 != 0) { // Es impar
+			int medio = Math.round(largo / 2);
+			return word.substring(medio, medio + 1);
+		} else {
+			return word.substring(largo / 2 - 1, largo / 2 + 1);
+		}
+
+	}
+
+	// OR
+
+	public static String getMiddle2(String word) {
+		int length = word.length();
+		return (length % 2 != 0) ? String.valueOf(word.charAt(length / 2))
+				: word.substring(length / 2 - 1, length / 2 + 1);
+	}
+
+	/*
+	 * Simple, given a string of words, return the length of the shortest word(s).
+	 * String will never be empty and you do not need to account for different data
+	 * types.
+	 */
+
+	public static int findShort(String s) {
+		String[] arr = s.split(" ");
+		String minimo = arr[0];
+
+		for (String word : arr) {
+			if (minimo.length() > word.length()) {
+				minimo = word;
+			}
+		}
+
+		return minimo.length();
+	}
+
+	/*
+	 * Complete the solution so that the function will break up camel casing, using
+	 * a space between words.
+	 * 
+	 * Example "camelCasing" => "camel Casing" "identifier" => "identifier" "" => ""
+	 */
+
+	public static String camelCase(String input) {
+		String cadena = "";
+
+		if (input.length() == 0) {
+			return input;
+		}
+
+		for (String word : input.split("")) {
+			if (Character.isUpperCase(word.charAt(0))) {
+				cadena += " " + word;
+			} else {
+				cadena += word;
+			}
+		}
+
+		return cadena;
+	}
+
+	// OR
+
+	public static String camelCase2(String input) {
+		return input.replaceAll("([A-Z])", " $1"); // Java tiene replaceAll
+	}
+
+	/*
+	 * Return the number (count) of vowels in the given string.
+	 * 
+	 * We will consider a, e, i, o, u as vowels for this Kata (but not y).
+	 */
+
+	// Utilizando import java.util.Arrays;
+
+	public static int getCount(String str) {
+		int vowelsCount = 0;
+
+		String[] arr = { "a", "e", "i", "o", "u" };
+
+		for (String word : str.split("")) {
+			if (Arrays.asList(arr).contains(word)) {
+				vowelsCount++;
+			}
+		}
+
+		return vowelsCount;
+	}
+
+	// OR
+
+	public static int getCount2(String str) {
+		return str.replaceAll("[^aeiou]", "").length(); // Remplazar todas menos las aeiou por un vacio y le saco el length
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println(5 << 12 == 5 * Math.pow(2, 12));
+
+		System.out.println(5 >> 2); // Si el resultado de a / 2^b --> a >> b es negativo return 0 sino return trunc
+									// del numero
+
+		System.out.println(5 | 11);
 
 	}
 
